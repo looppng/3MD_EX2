c81acd8705848abd98edf87589092e888fc1a071
Egils <63558651+looppng@users.noreply.github.com>
2023-11-09 11:51:28 +0200

    +burger

--- boilerplate/src/scripts.ts
+++ boilerplate/src/scripts.ts
@@ -6,3 +6,16 @@ console.log('Ready for coding');
 console.log('Body jQuery node:', $('body'));
 console.log('Body javascript node:', document.querySelector('body'));
 console.log('2 + 3 =', sum(2, 3));
+
+
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('.nav__menu');
+
+burger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+
+document.querySelectorAll(".nav__item").forEach( n => n.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+}));
